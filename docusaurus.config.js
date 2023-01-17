1c7d2796cbc5756e3fcba0cb0ef9139bc75f2c5c
whoisltd <tiendat.le129@gmail.com>
2023-01-17 11:09:55 +0700

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Second Brain',
   tagline: '',
   url: 'https://whoisltd.me',
-  baseUrl: '/my-second-brain/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
