c23db393c2c43da81aabcac514877f577337bfc3
whoisltd <tiendat.le129@gmail.com>
2023-01-17 11:11:35 +0700

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'My Second Brain',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
