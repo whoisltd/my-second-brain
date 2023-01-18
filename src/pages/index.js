a1758c0ff7a525ce8322b2319a67952d09940b01
whoisltd <tiendat.le129@gmail.com>
2023-01-18 13:53:58 +0700

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs">
+            to="/docs/intro">
             Read now ⏱️
           </Link>
         </div>
