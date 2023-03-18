9fd1ce0c6e2807db5148df09b3aced590904378f
WhoIsLTD <tiendat.le129@gmail.com>
2023-03-18 15:53:50 +0700

    update config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/whoisltd/my-second-brain/blob/main/docs/',
+            'https://github.com/whoisltd/my-second-brain/blob/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/whoisltd/my-second-brain/blob/main/docs/',
+            'https://github.com/whoisltd/my-second-brain/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
