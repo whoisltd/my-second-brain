5560acaaedcde7b11328c2a64ac6e65b7b10101a
whoisltd <tiendat.le129@gmail.com>
2026-03-31 11:29:30 +0700

    refactor: replace outdated graph design specs with a new implementation plan and update graph component configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Read now ⏱️
+            to="/docs/Introduction">
+            Want to see it? 🫣
           </Link>
         </div>
       </div>
